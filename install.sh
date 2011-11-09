9d05965e82d9b3500d10f07236bd1e9eebe9e893
Justyn Shull <justyn@justynshull.com>
2011-11-08 20:58:19 -0600

    Add colors to install.sh and detect pre-existing directories like .vim

--- install.sh
+++ install.sh
@@ -1,27 +1,32 @@
 #/bin/bash
 # Author:  Justyn Shull < justyn [at] justynshull.com >
-# Last Updated: 08/11/2011
+# Last Updated: 11/08/2011
 #
 # Script to link my dot files and configs to where they belong
 
 
 dotdir=$(pwd)
 ignore="README.md .git install.sh"
+. bash/colors
 for file in *; 
 do
     if [[ "$ignore" =~ "$file" ]]; then
         # ignore file
         echo -n
     else
-        if [ -f $HOME/.$file ] && [ ! -L $HOME/.$file ]; then
-            echo "$file: Exists but not a symlink. You should backup and delete(or move) it."
+        if [ -f $HOME/.$file ] && [ ! -L $HOME/.$file ] && [ -d $HOME/.$file ]; then
+            echo -e "${RedF}$file${reset}: Exists but not a symlink. You should backup and delete(or move) it."
             continue
         fi
         if [ -h $HOME/.$file ]; then
-            echo "$file: symlink exists"
+            echo -e "${GreenF}.$file${reset}: symlink exists"
         else
-            echo "$file: Linking to $HOME/.$file"
-            ln -s $dotdir/$file $HOME/.$file
+	    if [ -d $HOME/.$file ]; then
+		    echo -e "${RedF}.$file${reset}: is a directory. You should backup and delete(or move) it."
+	    else
+            	    echo -e "${YellowF}$file${reset}: Linking to ${BoldOn}$HOME/.$file${reset}"
+                    ln -s $dotdir/$file $HOME/.$file
+	    fi
         fi
     fi
 done
