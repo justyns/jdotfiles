b8c38c9244b08a635543d2cb65db968a1a2cae7c
Justyn Shull <justyn@justynshull.com>
2011-11-15 19:39:38 -0600

    Add support for bin/ directory to install.sh and also check if a few commands exist

--- install.sh
+++ install.sh
@@ -3,10 +3,18 @@
 # Last Updated: 11/08/2011
 #
 # Script to link my dot files and configs to where they belong
+. bash/colors
+
+#Check and see if these commands exist on the system
+reqcommands="vim git ruby screen tmux"
+for com in $reqcommands;
+do
+	hash ${com} 2>&- || echo -e >&2 "${RedF}${com}${reset}: not installed"
+done
 
 
 dotdir=$(pwd)
-ignore="README.md .git install.sh"
+ignore="README.md .git install.sh bin"
 . bash/colors
 for file in *; 
 do
@@ -30,3 +38,33 @@ do
         fi
     fi
 done
+
+#Link files in the ~bin directory
+#TODO: make a function instead of copying/pasting the code above :)
+
+ignore="README.md .git install.sh bin"
+cd ${dotdir}/bin
+mkdir -pv ${HOME}/bin
+for file in *;
+do
+    if [[ "$ignore" =~ "$file" ]]; then
+        # ignore file
+        echo -n
+    else
+        if [ -f $HOME/bin/$file ] && [ ! -L $HOME/bin/$file ]; then
+            echo -e "${RedF}$file${reset}: Exists but not a symlink. You should backup and delete(or move) it."
+            continue
+        fi
+        if [ -h $HOME/bin/$file ]; then
+            echo -e "${GreenF}bin/$file${reset}: symlink exists"
+        else
+            if [ -d $HOME/bin/$file ]; then
+                    echo -e "${RedF}bin/$file${reset}: is a directory. You should backup and delete(or move) it."
+            else
+                    echo -e "${YellowF}bin/$file${reset}: Linking to ${BoldOn}$HOME/bin/$file${reset}"
+                    ln -s $dotdir/bin/$file $HOME/bin/$file
+            fi
+        fi
+    fi
+done
+
