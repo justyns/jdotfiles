b250e8e3b48e1e60a91e6504bcaf5b5b3160a0f0
Justyn Shull <justyn@justynshull.com>
2011-11-15 19:56:03 -0600

    Added script to install dropbox on new servers - added link to init script

--- bin/install-dropbox.sh
+++ bin/install-dropbox.sh
@@ -3,6 +3,7 @@
 # Extremely simple script, but makes it so I dont have to remember to go to the dropbox website and find the link to download
 # Does not include an init script.
 # Linux download/install link:  https://www.dropbox.com/install?os=lnx
+# Gentoo init script: http://forums.gentoo.org/viewtopic-t-891740-view-previous.html?sid=d67ec8d0721a3178bb57c1eedd4c3afc
 
 cd $HOME
 if [ `getconf LONG_BIT` == '64' ];
