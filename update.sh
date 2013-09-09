5bfac50d531cd6801f86db4f1835b5730deb09c6
Justyn Shull <justyns@codero.com>
2013-09-09 09:30:37 -0500

    Updated vim submodules and add instructions on how to update them in the future.

--- update.sh
+++ update.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 # Author:  Justyn Shull < justyn [at] justynshull.com >
-# Last Updated: 07/15/2012
+# Last Updated: 09/09/2013
 #
 # Script that updates the files from my jdotfiles repo and also updates all git submodules
 # https://github.com/justyns/jdotfiles
 
 # Assumes we are already in the location of .jdotfiles
+# To update git submodules to their latest version:
+#   git submodule foreach git pull origin master
+#   git commit -a
 
 echo "Updating main repo"
 git pull
