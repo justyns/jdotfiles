2e1f68d707bd2c4ef2312b372ffadca2054b3410
Justyn Shull <git@justyn.io>
2016-04-19 18:12:16 -0500

    Fix network-time script for osx since -setnetworktimeserver didn't accept multiple time servers

--- bin/osx-set-network-time.sh
+++ bin/osx-set-network-time.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
 # Sets ntp servers in osx and makes sure the clock is up to date
-echo "Old time server:"
+echo "Old primary time server:"
 /usr/sbin/systemsetup -getnetworktimeserver
-/usr/sbin/systemsetup -setnetworktimeserver time.apple.com,0.us.pool.ntp.org,1.us.pool.ntp.org,2.us.pool.ntp.org,3.us.pool.ntp.org
-echo "New time server:"
+/usr/sbin/systemsetup -setnetworktimeserver time.apple.com
+/bin/cat >> /etc/ntp.conf <<EOF
+server 0.us.pool.ntp.org
+server 1.us.pool.ntp.org
+server 2.us.pool.ntp.org
+server 3.us.pool.ntp.org
+EOF
+echo "Updated /etc/ntp.conf"
+echo "New primary time server:"
 /usr/sbin/systemsetup -getnetworktimeserver
 echo "Syncing time"
 /usr/sbin/systemsetup -setusingnetworktime off
