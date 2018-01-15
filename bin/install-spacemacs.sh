88fb1bfdcdefce58a120d8fe937574a388468a1b
Justyn Shull <git@justyn.io>
2018-01-15 01:28:07 -0600

    Add ultisnips and vim-ledger settings

--- bin/install-spacemacs.sh
+++ bin/install-spacemacs.sh
@@ -5,3 +5,11 @@
 [[ -f ~/.emacs.d ]] && mv ~/.emacs.d-backup-$(date +%s)
 
 git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
+
+if [ -x $(which go) ]; then
+    go get -u -v github.com/nsf/gocode
+    go get -u -v github.com/rogpeppe/godef
+    go get -u -v golang.org/x/tools/cmd/oracle
+    go get -u -v golang.org/x/tools/cmd/gorename
+    go get -u -v golang.org/x/tools/cmd/goimports
+fi
