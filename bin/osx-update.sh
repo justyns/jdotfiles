bba0bb76d0e760ebf2b8c2bbab5ce2c187816fe9
Justyn Shull <git@justyn.io>
2018-11-08 09:50:36 -0600

    Update oh-my-zsh in osx-update.sh

--- bin/osx-update.sh
+++ bin/osx-update.sh
@@ -5,7 +5,7 @@ set -x
 echo "Updating brew"
 brew update; brew upgrade;
 echo "Updating brew cask"
-brew cask update; # brew cask upgrade;
+brew cask outdated; # brew cask upgrade
 echo "Cleaning up brew and brew cask"
 brew cleanup;brew cask cleanup
 
@@ -16,6 +16,11 @@ echo "Updating oh my fish"
 if [ -x `which fish` ]; then
     fish -c 'omf update'
     fish -c fish_update_completions
+fi
+
+echo "Updating oh-my-zsh"
+if [[ ! -z "$ZSH" ]]; then
+    upgrade_oh_my_zsh
 fi
 
 echo "Updating atom"
