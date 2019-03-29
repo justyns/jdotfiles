0b515918281a5a170d7eda1369da64e3edc75f14
Justyn Shull <git@justyn.io>
2019-03-29 17:18:05 -0500

    Add git squash alias

--- bash/auto.d/00_git.sh
+++ bash/auto.d/00_git.sh
@@ -2,3 +2,4 @@
 
 alias master='git checkout master && git pull'
 alias masterr='git stash && git checkout master && git pull && git stash apply'
+alias squash='git rebase -i $(git merge-base master HEAD)'
