fc70c7e9d3c2553dbb359beec25fdec229b2cf30
Justyn Shull <git@justyn.io>
2020-05-01 18:24:03 -0500

    Remove some stuff from my update script

--- bin/osx-update.sh
+++ bin/osx-update.sh
@@ -12,21 +12,24 @@ brew cleanup;brew cask cleanup
 echo "Updating my dotfiles"
 cd ~/.jdotfiles/ && ./update.sh
 
-echo "Updating oh my fish"
-if [ -x `which fish` ]; then
-    fish -c 'omf update'
-    fish -c fish_update_completions
-fi
+# 2020-05-01: I don't use fish anymore
+# echo "Updating oh my fish"
+# if [ -x `which fish` ]; then
+#     fish -c 'omf update'
+#     fish -c fish_update_completions
+# fi
 
 echo "Updating oh-my-zsh"
 if [[ ! -z "$ZSH" ]]; then
     upgrade_oh_my_zsh
 fi
 
-echo "Updating atom"
-if [ -x `which apm` ]; then
-    apm update --confirm=false
-fi
+# 2020-05-01: I don't really use atom anymore
+# echo "Updating atom"
+# if [ -x `which apm` ]; then
+#     apm update --confirm=false
+# fi
 
-echo "Updating OSX"
-softwareupdate -i -a
+# 2020-05-01: Disabling this for now so that I can control this separately
+# echo "Updating OSX"
+# softwareupdate -i -a
