a023fb02e78cb57bdce12610e5e20c6a5fad4363
Justyn Shull <git@justyn.io>
2020-07-15 13:32:51 -0500

    Add alias to cd to the root of a git repo

--- bash/auto.d/00_git.sh
+++ bash/auto.d/00_git.sh
@@ -3,3 +3,4 @@
 alias master='git checkout master && git pull'
 alias masterr='git stash && git checkout master && git pull && git stash apply'
 alias squash='git rebase -i $(git merge-base master HEAD)'
+alias gitroot='cd $(git root)'
