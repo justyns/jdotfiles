01c8692be8a312e993f2ffb5b950ac00052fc58b
Justyn Shull <git@justyn.io>
2025-08-17 17:28:35 -0500

    Update a couple old functions to use https

--- bash/auto.d/00_colors.sh
+++ bash/auto.d/00_colors.sh
@@ -4,7 +4,7 @@ if [ -x /usr/bin/dircolors ]; then
     alias ls='ls --color=auto'
     alias grep='grep --color=auto'
     alias fgrep='fgrep --color=auto'
-    alias egrep='egrep --color=auto'
+    alias egrep='grep --color=auto -E'
 fi
 
 # Source color definitions
