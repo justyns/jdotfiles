f3cdf92b945720c32319f2bc3c71aac7e041c035
Justyn Shull <git@justyn.io>
2025-08-31 19:42:13 -0500

    Set wezterm color scheme

---
+++ wezterm.lua
@@ -0,0 +1,10 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+config.color_scheme = "Catppuccin Frappe"
+
+-- Finally, return the configuration to wezterm:
+return config
