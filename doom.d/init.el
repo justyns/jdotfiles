2162e9442e800746070c9d3f0f16307f9447b4f7
Justyn Shull <git@justyn.io>
2026-07-11 03:39:38 -0500

    doom: Enable magit forge

--- doom.d/init.el
+++ doom.d/init.el
@@ -103,7 +103,7 @@
        lookup              ; navigate your code and its documentation
        ;;llm               ; when I said you needed friends, I didn't mean...
        ;;(lsp +eglot)      ; M-x vscode
-       magit             ; a git porcelain for Emacs
+       (magit +forge)             ; a git porcelain for Emacs
        ;;make              ; run make tasks from Emacs
        ;;pass              ; password manager for nerds
        ;;pdf               ; pdf enhancements
